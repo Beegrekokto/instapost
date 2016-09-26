ec41694c2eb306d9b47ff256ebdb3ace1d00434f
Aashik <beegrekokto@gmail.com>
2016-09-26 23:45:26 +0545

    Lesson 14

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
 	def new
        @post = Post.new
+	end
+
+	def create
+	  @post = current_user.posts.create(post_params)
+	  if @post.valid?
+	  	redirect_to root_path
+	  else
+	  	render :new, status: :unprocessable_entity
+		end
+	end
+
+    def index
+    	@posts = Post.all.order('created_at DESC')	
+    	
+    end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:user_id, :photo, :description)
+		
 	end
 end
