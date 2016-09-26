3c884c7c9ccf3173a8cf3dc9cc9a672b72b3d273
Aashik <beegrekokto@gmail.com>
2016-09-27 00:39:10 +0545

    lesson 15

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,22 @@ class PostsController < ApplicationController
     	
     end
 
+    def edit
+    	@post = Post.find(params[:id])
+    	
+    end
+
+    def update
+    	@post = Post.find(params[:id])
+    	@post.update(post_params)
+    	if @post.valid?
+    		redirect_to root_path
+    	else
+    		render :new, status: :unprocessable_entity
+    	end
+    	
+    end
+
 	private
 
 	def post_params
